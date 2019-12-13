d992360f991bb0a128a9ea2af2fa095527e95c0e
iBlurBlur <tanakorn.ngam@gmail.com>
2019-12-13 12:04:39 +0700

    ....

--- mypos/src/app/app-routing.module.ts
+++ mypos/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/authen/login/login.component';
+import { RegisterComponent } from './components/authen/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', redirectTo: 'login'},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
